51f298e535fddecd814bd9520ee7a91abf249047
oxoxoxIxoxoxo <142164978+oxoxoxIxoxoxo@users.noreply.github.com>
2024-02-23 22:38:09 +0100

    Add files via upload

---
+++ Bestimmung_Der_Loschmidtzahl.py
@@ -0,0 +1,23 @@
+from random import choice
+
+def frage(d):
+    a = choice(list(d.keys()))
+    print("Wie lautet die allgemeine", a, "?")
+    aw = input("Die allgemeine {} lautet: ".format(a))
+    if aw not in d[a]:
+        print("Falsch!")
+        print("Die allgemeine", a, "lautet:", ', '.join(d[a]))
+    else:
+        print("Richtig!")
+        del d[a]
+
+# Dictionary mit LaTeX-Gleichungen
+dicA = {
+    'Gasgleichung': ['p·V=N·k·T'],
+    'Gaskonstante': ['R=N_A·k']
+}
+
+while dicA:
+    frage(dicA)
+
+print("Sie beherrschen die Bestimmung aus der Gasgleichung!")
