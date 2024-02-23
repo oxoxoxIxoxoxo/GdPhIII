facc605b8adc74f647de7e4eb82d830471daa603
oxoxoxIxoxoxo <142164978+oxoxoxIxoxoxo@users.noreply.github.com>
2024-02-23 22:41:43 +0100

    Messung_der_Gaskonstanten.py

--- Messung_der_Gaskonstanten.py
+++ Messung_der_Gaskonstanten.py
@@ -25,4 +25,4 @@ dicA = {
 while dicA:
     frage(dicA)
 
-print("Sie beherrschen die Bestimmung aus der Gasgleichung!")
+print("Sie beherrschen die Bestimmung der Gaskonstante aus der Gasgleichung!")
